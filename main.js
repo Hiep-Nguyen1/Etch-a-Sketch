39d59d53887a901e79727e9234a0f79eb7f216d5
Hiep Nguyen <122915998+Hiep-Nguyen1@users.noreply.github.com>
2023-07-10 14:23:35 -0500

    Complete grid creation function

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 function createGrid (rows, columns){
     const container = document.querySelector('.container');
-    container.setAttribute('style','display: flex; flex-direction: column; flex: 1 1 100%; justify-content: space-evenly;'); 
+    container.setAttribute('style','display: flex; flex-direction: column; flex: 1 1 auto; justify-content: space-evenly; height: 100%;'); 
 
     for(let i = 0; i < rows; i++){ //creates the rows of the grid
         const row = document.createElement('div'); 
         row.classList.add('grid','row');
-        row.style.cssText = 'display: flex; flex-direction: row; flex: 1 1 auto; justify-content: space-evenly;'
-
+        
         for(let j=0; j<columns;j++){ //creates columns of grid
             const col = document.createElement('div');
-            col.classList.add('grid','column');
+            col.classList.add('grid','column','square');
             row.appendChild(col);
         }
         
-        container.appendChild(row);
+        container.appendChild(row);        
     }
 
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => square.style.cssText = 'background-color: blue; border-style: solid; border-color: red; height: 100%; width: 100%')
 
-
-    const grid = document.querySelectorAll('.grid');
-    grid.forEach((square) => {
-        square.style.cssText = 'background-color: blue; border-style: solid; border-color: red;'
-    })
+    const row = document.querySelectorAll('.row');
+    row.forEach((r) => r.style.cssText = 'display: flex; flex: 1 1 auto; justify-content: space-evenly; height: 100%;')
 }
 
 
