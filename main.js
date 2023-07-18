485bc30a22f8ee589a7f1d650f2d8af6352ffda3
Hiep Nguyen <122915998+Hiep-Nguyen1@users.noreply.github.com>
2023-07-18 15:44:25 -0500

    Add drawing features and removed colors and border on grid

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const drawpad = document.querySelector('.container');
+
 function createGrid (rows, columns){
-    const container = document.querySelector('.container');
-    container.setAttribute('style','display: flex; flex-direction: column; flex: 1 1 auto; justify-content: space-evenly; height: 100%;'); 
+    drawpad.setAttribute('style','display: flex; flex-direction: column; flex: 1 1 auto; justify-content: space-evenly; border-style: solid; border-color: gray;'); 
 
     for(let i = 0; i < rows; i++){ //creates the rows of the grid
         const row = document.createElement('div'); 
@@ -12,15 +13,37 @@ function createGrid (rows, columns){
             row.appendChild(col);
         }
         
-        container.appendChild(row);        
+        drawpad.appendChild(row);        
     }
-
     const squares = document.querySelectorAll('.square');
-    squares.forEach((square) => square.style.cssText = 'background-color: blue; border-style: solid; border-color: red; height: 100%; width: 100%')
+    squares.forEach((square) => square.style.cssText = 'border: none; height: 100%; width: 100%')
 
     const row = document.querySelectorAll('.row');
     row.forEach((r) => r.style.cssText = 'display: flex; flex: 1 1 auto; justify-content: space-evenly; height: 100%;')
 }
 
 
-createGrid(4,4);
+
+function draw () {                                          //Activates drawing only when mouse is held down
+    drawpad.addEventListener("mousedown", (e) => hold());
+    drawpad.addEventListener("mouseup", (e) => release());
+    drawpad.addEventListener("mouseleave", (e) => release());
+}
+
+function hold () {
+    squares.forEach((square) => square.addEventListener("mouseenter", enter)); 
+}
+
+function enter () {
+    this.style.backgroundColor = 'black';
+}
+
+function release () {
+    squares.forEach((square) => square.removeEventListener("mouseenter", enter)); 
+}
+
+
+
+createGrid(10,10);
+const squares = document.querySelectorAll('.square');
+draw();
