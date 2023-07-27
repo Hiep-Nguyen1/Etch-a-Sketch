0f9a70becb1ba50a81d626556cd8e5b370d3a056
Hiep Nguyen <122915998+Hiep-Nguyen1@users.noreply.github.com>
2023-07-27 12:12:05 -0500

    Add grid change function and refactored how buttons initiate'

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const drawpad = document.querySelector('.drawPad');
+let squares = '';
+let color = 'white';
 
 function createGrid (rows, columns){
-    drawpad.setAttribute('style','display: flex; flex-direction: column; flex: 0 1 auto; justify-content: space-evenly; border-style: solid; border-color: gray;'); 
-
     for(let i = 0; i < rows; i++){ //creates the rows of the grid
         const row = document.createElement('div'); 
         row.classList.add('grid','row');
@@ -22,12 +22,10 @@ function createGrid (rows, columns){
     row.forEach((r) => r.style.cssText = 'display: flex; flex: 1 1 auto; justify-content: space-evenly; height: 100%;')
 }
 
-
-
-function draw () {                                          //Activates drawing only when mouse is held down
-    drawpad.addEventListener("mousedown", () => hold());
-    drawpad.addEventListener("mouseup", () => release());
-    drawpad.addEventListener("mouseleave", () => release());
+function draw () { //Activates drawing only when mouse is held down
+    drawpad.addEventListener("mousedown", hold);
+    drawpad.addEventListener("mouseup", release);
+    drawpad.addEventListener("mouseleave", release);
 }
 
 function hold () {
@@ -50,15 +48,45 @@ function clear () {
     squares.forEach((square) => square.style.backgroundColor = 'white')
 }
 
-let color = 'white';
+function gridResize(){
+    while (drawpad.firstChild) { //clears drawpad of all child elements
+        drawpad.removeChild(drawpad.firstChild);
+    }
+    let size = (function () { //Will read input from number and assign that value to size
+        const size = document.querySelector('.gridSize');
+        if (size.value <= 0) {
+            alert("Please enter a valid size for your canvas.");
+        }
+        else if (size.value > 100) {
+            alert("Woah any size bigger than 100 is too large. Please enter a lower value.")
+        }
+        else {
+            return size.value;
+        }
+    })();
+
+    createGrid(size,size);
+    updateSquare();
+}
+
+function updateSquare () { //will set squares variable to elements with current square class
+    squares = document.querySelectorAll('.square');
+}
 
 createGrid(30,30);
-const squares = document.querySelectorAll('.square');
+updateSquare();
+ 
+(function () { //IIFE to set event listeners without populating global space
+    drawpad.setAttribute('style','display: flex; flex-direction: column; flex: 0 1 auto; justify-content: space-evenly; border-style: solid; border-color: gray;');
+    const btns = document.querySelectorAll('.colorChange');
+    btns.forEach((btn) => btn.addEventListener('click', (e) => black()));
+
+    const btnClear = document.querySelector('.clear');
+    btnClear.addEventListener('click', clear);
+
+    const btnChangeGrid = document.querySelector('.changeGrid');
+    btnChangeGrid.addEventListener('click',gridResize);
+})();
+
 draw();
-
-const btns = document.querySelectorAll('.colorChange');
-btns.forEach((btn) => btn.addEventListener('click', (e) => black()));
-
-const btnClear = document.querySelector('.clear');
-btnClear.addEventListener('click', clear);
 
